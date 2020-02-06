3abecdb3c26cd28211ffcd309ab6f41d68017ba3
Shaah Cooper <shaahcooper3@flakko.local>
2020-02-06 14:10:59 -0800

    printBoard

--- games/checkers/createNewBoard.js
+++ games/checkers/createNewBoard.js
@@ -1,38 +1,38 @@
 let classes = require('./classes');
 
 let initialPositions = {
-  'B1': [0,1],
-  'B2': [0,3],
-  'B3': [0,5],
-  'B4': [0,7],
-  'B5': [1,0],
-  'B6': [1,2],
-  'B7': [1,4],
-  'B8': [1,6],
-  'B9': [2,1],
-  'B10': [2,3],
-  'B11': [2,5],
-  'B12': [2,7],
-  'R1': [5,0],
-  'R2': [5,2],
-  'R3': [5,4],
-  'R4': [5,6],
-  'R5': [6,1],
-  'R6': [6,3],
-  'R7': [6,5],
-  'R8': [6,7],
-  'R9': [7,0],
-  'R10': [7,2],
-  'R11': [7,4],
-  'R12': [7,6],
+  'B1': [0, 1],
+  'B2': [0, 3],
+  'B3': [0, 5],
+  'B4': [0, 7],
+  'B5': [1, 0],
+  'B6': [1, 2],
+  'B7': [1, 4],
+  'B8': [1, 6],
+  'B9': [2, 1],
+  'B10': [2, 3],
+  'B11': [2, 5],
+  'B12': [2, 7],
+  'R1': [5, 0],
+  'R2': [5, 2],
+  'R3': [5, 4],
+  'R4': [5, 6],
+  'R5': [6, 1],
+  'R6': [6, 3],
+  'R7': [6, 5],
+  'R8': [6, 7],
+  'R9': [7, 0],
+  'R10': [7, 2],
+  'R11': [7, 4],
+  'R12': [7, 6],
 }
 
 function createNewBoard() {
   let board = new classes.Board();
   // Add places to board
-  for(let i = 0; i<8; i++){
-    for(let j = 0; j<8; j++){
-      board.addPlace(new classes.Place(i,j));
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      board.addPlace(new classes.Place(i, j));
     }
   }
   // Add Pieces to board
@@ -62,4 +62,5 @@ function createNewBoard() {
 
 let board = createNewBoard();
 
-console.log(board);
+
+module.exports = createNewBoard
